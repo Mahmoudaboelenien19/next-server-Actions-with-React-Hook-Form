5f9a1813a50d4c78b17eec82c2ab3e6412bd6660
unknown <103467245+Mahmoudaboelenien19@users.noreply.github.com>
2023-11-07 01:49:23 +0200

    *

--- app/actions.ts
+++ app/actions.ts
@@ -1,7 +1,7 @@
 "use server";
 import { connectToMongoose } from "@/lib/mongoose/connectToMongoose";
 import { userCollection } from "@/lib/mongoose/schema";
-import { revalidatePath } from "next/cache";
+import { revalidatePath, revalidateTag } from "next/cache";
 import { NextResponse } from "next/server";
 export const AddTodo = async (data: any) => {
   await connectToMongoose();
@@ -10,8 +10,7 @@ export const AddTodo = async (data: any) => {
   //   const { name, age, email } = Object.fromEntries(data);
 
   const user = await userCollection.create(data);
-  revalidatePath("/api/users");
-
+  revalidateTag("users");
   return {
     status: 200,
   };
--- lib/getUsers.ts
+++ lib/getUsers.ts
@@ -1,5 +1,7 @@
 export const getUsers = async () => {
-  const res = await fetch("http://localhost:3000/api/users");
+  const res = await fetch("http://localhost:3001/api/users", {
+    next: { tags: ["users"] },
+  });
 
   if (!res.ok) return;
   const data = await res.json();
